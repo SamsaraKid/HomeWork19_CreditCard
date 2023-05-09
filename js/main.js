bfb5e01d0f6b07344ae43029826d6d2ea9d841b6
koany <koroliev@yandex.ru>
2023-05-10 01:07:10 +0300

    Простой вариант ввода по 4 цифры в поле номера

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready($('.long').val(''))
 $(document).ready($('.short').val(''))
+let cardNum = 0
+const arr = []
+
 
 function myFunction() {
     // if ($('#number').prop('valid', true)) {
@@ -10,4 +13,48 @@ function myFunction() {
     //     $('#button').attr('disabled', 'disabled')
     // }
     $('#name').val($('#name').val().toUpperCase())
+
+    // let inNum = $('#number').val()
+    // let len = inNum.length
+    // // let num = inNum.replaceAll(/\s+/g, '')
+    // let num = inNum.replaceAll(' ', '')
+    //
+    // switch (num.length) {
+    //     case 4: arr.push(num.substring(0,4)); break;
+    //     case 8: arr.push(num.substring(4,8)); break;
+    //     case 12: arr.push(num.substring(8,12)); break;
+    //     case 16: arr.push(num.substring(12,16)); break;
+    // }
+    // // const arr = [num.substring(0,4),num.substring(4,8),num.substring(8,12),num.substring(12,16)]
+    // console.log(arr)
+    // console.log(arr.join(' '))
+    // $('#number').val(arr.join(' '))
+    // $('#number').val(arr.join(' '))
+    // console.log(arr.join(','))
+    // if (len == 4 || len == 9 || len == 14 || len == 16) {
+    //     $('#number').val(arr.join(','))
+    // }
+
+
+
+
+    let num = $('#number').val()
+    console.log('num = ' + num)
+    let len = num.length
+    console.log('len = ' + len)
+
+    if (len == 4 || len == 9 || len == 14) {
+        $('#number').val(num + ' ')
+        console.log()
+    }
+
+
 }
+
+$('#day').change(function (){
+    let num = $('#day').val()
+    let len = num.length
+    if (len == 1){
+        $('#day').val(0 + num)
+    }
+})
