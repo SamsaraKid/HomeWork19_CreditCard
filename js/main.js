63ac9fe3d1aa31b0c6e8661ffe7bf4fca62d8247
koany <koroliev@yandex.ru>
2023-05-09 19:56:52 +0300

    Первая готовность

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready($('.long').val(''))
+$(document).ready($('.short').val(''))
+
+function myFunction() {
+    // if ($('#number').prop('valid', true)) {
+    //     console.log('valid true')
+    //     $('#button').removeAttr('disabled')
+    // } else {
+    //     console.log('valid false')
+    //     $('#button').attr('disabled', 'disabled')
+    // }
+    $('#name').val($('#name').val().toUpperCase())
+}
