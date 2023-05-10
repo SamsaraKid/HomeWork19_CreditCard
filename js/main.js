f3bb53947d7d122637939baafcb9c6a699a735e9
koany <koroliev@yandex.ru>
2023-05-10 18:29:59 +0300

    Классно вышло с форматом ввода номера карты

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready($('.long').val(''))
 $(document).ready($('.short').val(''))
 let prevLen = 0
+let arr = []
 
 function myFunction() {
+
 
     $('#name').val($('#name').val().toUpperCase())
 
@@ -10,15 +12,27 @@ function myFunction() {
 
 
 
-    let num = $('#number').val()
-    let len = num.length
-    if ((len == 4 || len == 9 || len == 14) && len > prevLen) {
-        $('#number').val(num + ' ')
-    } else if (len == 16 && num.indexOf(' ') == -1) {
-        const arr = [num.substring(0,4),num.substring(4,8),num.substring(8,12),num.substring(12,16)]
-        $('#number').val(arr.join(' '))
+    // let num = $('#number').val()
+    // let len = num.length
+    // if ((len == 4 || len == 9 || len == 14) && len > prevLen) {
+    //     $('#number').val(num + ' ')
+    // } else if (len == 16 && num.indexOf(' ') == -1) {
+    //     const arr = [num.substring(0,4),num.substring(4,8),num.substring(8,12),num.substring(12,16)]
+    //     $('#number').val(arr.join(' '))
+    // }
+    // prevLen = len
+
+    let num = $('#number').val().replaceAll(' ','')
+    arr = [num.substring(0,4),num.substring(4,8),num.substring(8,12),num.substring(12,16)]
+    let firstEmpty = arr.indexOf('')
+    if (firstEmpty != -1) {
+        arr.splice(firstEmpty, 3)
     }
-    prevLen = len
+    $('#number').val(arr.join(' '))
+
+
+
+
 
 }
 
@@ -29,3 +43,7 @@ $('#day').change(function (){
         $('#day').val(0 + num)
     }
 })
+
+
+
+
